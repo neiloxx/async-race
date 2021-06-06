e4dcbd7f3080c9d224382fd62d7f6bcffb963bdc
neiloxx <anastasia.v.sido@gmail.com>
2021-06-06 18:25:22 +0600

    feat: add routing and update styles

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,13 +1,55 @@
 import Control from './components/elements/control';
+import Header from './components/header/header';
 import './style.css';
+import Garage from './pages/garage/garage';
+import Winners from './pages/winners/winners';
+import Settings from './pages/settings/settings';
 
 class App {
+  header: Control;
+
   main: Control;
 
   constructor(parent: HTMLElement) {
+    this.header = new Header(parent);
     this.main = new Control(parent, 'main', 'main', '');
-    console.log(this.main);
   }
 }
 
 const app = new App(document.body);
+
+const routerView = app.main.node;
+const links = document.querySelectorAll('.nav-list__item-link');
+
+const onRouteChanged = (): Control => {
+  const { hash } = window.location;
+  if (!(routerView instanceof HTMLElement)) {
+    throw new ReferenceError('No router view element available for rendering');
+  }
+  routerView.innerHTML = '';
+  links.forEach(link => {
+    if (link instanceof HTMLAnchorElement) {
+      if (
+        link.href.slice(link.href.lastIndexOf('#') + 1) ===
+        hash.slice(hash.lastIndexOf('#') + 1)
+      ) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    }
+  });
+  switch (hash) {
+    case '':
+      return new Garage(routerView);
+    case '#winners':
+      return new Winners(routerView);
+    case '#settings':
+      return new Settings(routerView);
+    default:
+      return new Garage(routerView);
+  }
+};
+
+window.addEventListener('hashchange', onRouteChanged);
+onRouteChanged();
