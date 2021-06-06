33ac08bdc5a07f9c05fcc04d6ed324933b550d5e
neiloxx <anastasia.v.sido@gmail.com>
2021-06-06 18:24:47 +0600

    feat: add header component

---
+++ async-race/src/components/header/header.ts
@@ -0,0 +1,23 @@
+import './style.css';
+import ControlArray from '../elements/control-array';
+import Link from '../elements/link/link';
+
+export default class Header extends ControlArray {
+  constructor(parent: HTMLElement) {
+    super(parent, 'header', 'header', [
+      new ControlArray(undefined, 'div', 'header__container', [
+        new ControlArray(undefined, 'nav', 'header__nav nav', [
+          new ControlArray(undefined, 'ul', 'nav-list', [
+            new ControlArray(undefined, 'li', 'nav-list__item', [
+              new Link(undefined, 'nav-list__item-link', '#', 'garage'),
+            ]),
+            new ControlArray(undefined, 'li', 'nav-list__item', [
+              new Link(undefined, 'nav-list__item-link', '#winners', 'winners'),
+            ]),
+          ]),
+        ]),
+        new Link(undefined, 'settings', '#settings'),
+      ]),
+    ]);
+  }
+}
