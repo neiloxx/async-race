a53658e61b9606c681b7d8ff40c7238f821076c4
neiloxx <anastasia.v.sido@gmail.com>
2021-06-10 15:52:51 +0600

    refactor: update config

--- async-race/webpack.config.js
+++ async-race/webpack.config.js
@@ -1,92 +1,90 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtraPlugin = require("mini-css-extract-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtraPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const devServer = isDev =>
   !isDev
     ? {}
     : {
-      devServer: {
-        open: true,
-        hot: true,
-        port: 8080,
-        contentBase: path.join(__dirname, "public")
-      }
-    };
+        devServer: {
+          open: true,
+          hot: true,
+          port: 8080,
+          contentBase: path.join(__dirname, 'public'),
+        },
+      };
 
 const esLintPlugin = isDev =>
-  isDev ? [] : [new ESLintPlugin({ extensions: ["ts", "js"] })];
+  isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
 
 module.exports = ({ develop }) => ({
-  mode: develop ? "development" : "production",
-  devtool: develop ? "inline-source-map" : false,
+  mode: develop ? 'development' : 'production',
+  devtool: develop ? 'inline-source-map' : false,
   entry: {
-    app: "./src/index.ts"
+    app: './src/index.ts',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    assetModuleFilename: "assets/[hash][ext]"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext]',
   },
   module: {
     rules: [
       {
         test: /\.[tj]s$/,
-        use: "ts-loader",
-        exclude: /node_modules/
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: "css-loader" },
+          { loader: 'css-loader' },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: [path.resolve(__dirname, "src/styles")]
-              }
-            }
-          }
-        ]
+                includePaths: [path.resolve(__dirname, 'src/styles')],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource"
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource"
-      }
-    ]
+        type: 'asset/resource',
+      },
+    ],
   },
 
   plugins: [
     new CopyPlugin({
-      patterns: [
-        { from: "./public" }
-      ]
+      patterns: [{ from: './public' }],
     }),
-    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: './src/index.html',
     }),
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new MiniCssExtraPlugin({
-      filename: "[name].css"
+      filename: '[name].css',
     }),
-    ...esLintPlugin(develop)
+    ...esLintPlugin(develop),
   ],
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js'],
   },
-  ...devServer(develop)
+  ...devServer(develop),
 });
