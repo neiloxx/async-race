da955a3e0acdef4301d05b05148a35f9981a7053
neiloxx <anastasia.v.sido@gmail.com>
2021-06-14 12:40:21 +0600

    feat: activate add car button

--- async-race/src/pages/garage/garage.ts
+++ async-race/src/pages/garage/garage.ts
@@ -3,27 +3,53 @@ import './style.scss';
 import ControlArray from '../../components/elements/control-array';
 import store from '../../store/store';
 import InputsField from './inputs-field/inputs-field';
+import CarsField from './cars-field/cars-field';
+import { createCar } from '../../api/cars';
 
 export default class Garage extends Control {
-  wrapper: ControlArray;
+  wrapper?: ControlArray;
 
-  inputsField: Control;
+  inputsField?: InputsField;
+
+  carsField?: CarsField;
+
+  title?: Control;
 
   constructor(parent: HTMLElement) {
     super(parent, 'section', 'garage');
-    this.inputsField = new InputsField(this.node, 'div', 'garage__inputs');
+    this.render();
+  }
 
-    const title = new Control(
+  startObserve(): void {
+    if (!this.inputsField) throw new Error("There's no render elements");
+    this.inputsField.renderBtn.createBtn.getNode().onclick = () => {
+      const newCar = this.inputsField?.getInputsValue();
+      if (!newCar?.name) throw new Error("There's no new car");
+      createCar(newCar).then(async () => {
+        await store.getValues();
+        this.node.innerHTML = '';
+        this.render();
+      });
+    };
+  }
+
+  render(): void {
+    this.inputsField = new InputsField(this.node, 'div', 'garage__inputs');
+    this.inputsField.watchInputs();
+    this.title = new Control(
       undefined,
       'h2',
       'garage__title',
       `garage(${store.carsCount})`,
     );
 
+    this.carsField = new CarsField(undefined, 'garage__cars-field');
+    this.startObserve();
+
     this.wrapper = new ControlArray(
       'div',
       'garage__container',
-      [title],
+      [this.title, this.carsField],
       this.node,
     );
   }
--- async-race/src/pages/garage/inputs-field/inputs-field.ts
+++ async-race/src/pages/garage/inputs-field/inputs-field.ts
@@ -2,11 +2,20 @@ import Control from '../../../components/elements/control';
 import ControlArray from '../../../components/elements/control-array';
 import { renderInputs, renderButtons } from './render-el/render-el';
 import './style.scss';
+import { ICar } from '../../../api/interfaces';
 
 export default class InputsField extends Control {
   createWrapper: ControlArray;
 
   updateWrapper: ControlArray;
+
+  renderInput = renderInputs();
+
+  renderBtn = renderButtons();
+
+  value = '';
+
+  color = '#000000';
 
   constructor(
     parent: HTMLElement,
@@ -14,17 +23,13 @@ export default class InputsField extends Control {
     className = '',
   ) {
     super(parent, tagName, className);
-
-    const renderInput = renderInputs();
-    const renderBtn = renderButtons();
-
     this.createWrapper = new ControlArray(
       'div',
       'garage__inputs-create',
       [
-        renderInput.createInputName,
-        renderInput.createInputColor,
-        renderBtn.createBtn,
+        this.renderInput.createInputName,
+        this.renderInput.createInputColor,
+        this.renderBtn.createBtn,
       ],
       this.node,
     );
@@ -32,11 +37,41 @@ export default class InputsField extends Control {
       'div',
       'garage__inputs-update',
       [
-        renderInput.updateInputName,
-        renderInput.updateInputColor,
-        renderBtn.updateBtn,
+        this.renderInput.updateInputName,
+        this.renderInput.updateInputColor,
+        this.renderBtn.updateBtn,
       ],
       this.node,
     );
+  }
+
+  watchInputs(): void {
+    this.renderInput.createInputName.getNode().oninput = (event: Event) => {
+      const input = <HTMLInputElement>event.target;
+      this.value = input.value;
+      this.activateBtn();
+      if (!this.value) {
+        this.blockBtn();
+      }
+    };
+    this.renderInput.createInputColor.getNode().oninput = (event: Event) => {
+      const input = <HTMLInputElement>event.target;
+      this.color = input.value;
+    };
+  }
+
+  getInputsValue(): ICar {
+    return {
+      name: this.value,
+      color: this.color,
+    };
+  }
+
+  activateBtn(): void {
+    this.renderBtn.createBtn.getNode().removeAttribute('disabled');
+  }
+
+  blockBtn(): void {
+    this.renderBtn.createBtn.getNode().setAttribute('disabled', '');
   }
 }
