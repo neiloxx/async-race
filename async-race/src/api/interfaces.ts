5f4452e63f72ed8b6c3bc6b9f4eb3124e50d851b
neiloxx <anastasia.v.sido@gmail.com>
2021-06-15 12:24:01 +0600

    feat: add interfaces for winners

--- async-race/src/api/interfaces.ts
+++ async-race/src/api/interfaces.ts
@@ -2,9 +2,19 @@ export interface IGetCarsResponse {
   items: Array<ICar>;
   count: number;
 }
+export interface IGetWinnersResponse {
+  items: Array<IWinner>;
+  count: number;
+}
 
 export interface ICar {
   name: string;
   color: string;
   id?: number;
+}
+
+export interface IWinner {
+  id: number;
+  wins: number;
+  time: number;
 }
