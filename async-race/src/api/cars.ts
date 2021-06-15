1fcd1cd4a1a11be655426d4f0b546ef640e126ea
neiloxx <anastasia.v.sido@gmail.com>
2021-06-15 12:25:29 +0600

    feat: add deleteWinners and getWinners functions

--- async-race/src/api/cars.ts
+++ async-race/src/api/cars.ts
@@ -1,8 +1,9 @@
 import { del, get, post, put } from './fetcher';
 import routes from './routes';
-import { ICar, IGetCarsResponse } from './interfaces';
+import { ICar, IGetCarsResponse, IGetWinnersResponse } from './interfaces';
 
 export const maxCarsOnPage = 7;
+export const maxWinnersOnPage = 10;
 
 export const getCars = async (
   page: number,
@@ -22,10 +23,33 @@ export const updateCar = async (id: number, body: ICar): Promise<void> => {
   await put(`${routes.garage}/${id}`, body);
 };
 
+export const getCar = async (id: number): Promise<ICar> => {
+  const response = await fetch(`${routes.garage}/${id}`);
+  return response.json();
+};
+
 export const createCar = async (body: ICar): Promise<void> => {
   await post(`${routes.garage}`, body);
 };
 
 export const deleteCar = async (id: number): Promise<void> => {
   await del(`${routes.garage}/${id}`);
+};
+
+export const deleteWinner = async (id: number): Promise<void> => {
+  await del(`${routes.winners}/${id}`);
+};
+
+export const getWinners = async (
+  page: number,
+  limit: number = maxWinnersOnPage,
+): Promise<IGetWinnersResponse> => {
+  return get(`${routes.winners}?_page=${page}&_limit=${limit}`).then(
+    async res => {
+      return {
+        items: await res.json(),
+        count: +(res.headers.get('X-Total-Count') || 0),
+      };
+    },
+  );
 };
